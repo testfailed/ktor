32badf5c2efd2ba87580648e80acc32385dc0b71
anastasiia.spaseeva <anastasiiia.spaseeva@jetbrains.com>
2021-10-14 17:07:29 +0200

    Add kotlin/dev space repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,7 @@ buildscript {
         mavenCentral()
         google()
         gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
     }
 
     val kotlin_version: String by extra
